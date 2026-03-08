5ddbc4b6ea2e1d00928575cfdc343831362ba00e
Mick <mvelden6@gmail.com>
2026-03-08 15:07:46 +0100

    calculate_predicted_time() verbeteren + wat UI verbeteringen

--- logic/performances.py
+++ logic/performances.py
@@ -6,102 +6,109 @@ from rich.table import Table
 
 from utils import to_seconds, format_timedelta
 
+console = Console()
 
-def calculate_performances():
+def calculate_performances() -> None:
     # print("vergelijkbare prestaties berekenen is nog niet geimplementeerd...\n")
     print("Hier kun je vergelijkbare prestaties berekenen bijv. op basis van een recent gelopen pr.\n"
           "De afstanden die worden berekend zijn 5km, 10km, halve marathon en de marathon.\n")
 
+    distances = {
+        1: {"km": 5, "label": "5km", "example": "20:00"},
+        2: {"km": 10, "label": "10km", "example": "40:00"},
+        3: {"km": 21.0975, "label": "Halve marathon", "example": "1:30:00"},
+        4: {"km": 42.195, "label": "Marathon", "example": "3:00:00"},
+    }
+
     while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        menu_items = {
-            1: {"id": "5km"},
-            2: {"id": "10km"},
-            3: {"id": "Halve marathon"},
-            4: {"id": "Marathon"},
-            5: {"id": "Terug naar menu"},
-        }
-        distances = {
-            1: {"km": 5, "label": "5km", "example": "20:00"},
-            2: {"km": 10, "label": "10km", "example": "40:00"},
-            3: {"km": 21.0975, "label": "Halve marathon", "example": "1:30:00"},
-            4: {"km": 42.195, "label": "Marathon", "example": "3:00:00"},
-            0: {"label": "Terug naar menu"}
-        }
-
-        for index, menu_item in menu_items.items():
-            print(f"{index}) {menu_item['id']}")
-
-        # print("1) 5km")
-        # print("2) 10km")
-        # print("3) halve marathon")
-        # print("4) marathon")
-        # print("0) Terug naar menu")
-
-        try:
-            choice = int(input("\nMaak een keuze uit een van de opties: "))
-
-            if choice in distances and choice != 0:
-                from_distance = distances[choice].get('km')
-                print(from_distance)
-                distance_label = distances[choice].get('label')
-                print(distance_label)
-                example_distance = distances[choice].get('example')
-                print(example_distance)
-
-                user_time = input(f"Voer een {distance_label} tijd in. voorbeeld: {example_distance}\n")
-
-                predicted_times = calculate_predicted_time(user_time, from_distance)
-
-                predicted_times_to_table(predicted_times)
+        # os.system('cls' if os.name == 'nt' else 'clear')
 
 
-        except ValueError:
-            print("Dit is geen geldige invoer")
+        show_menu(distances)
 
+        choice = get_user_choice()
+
+        if choice not in distances:
+            console.print(f"[bold red]Dit is geen geldige invoer[/bold red]\n")
+            continue
+
+        if choice in distances:
+            from_distance = distances[choice].get('km')
+            distance_label = distances[choice].get('label')
+            example_distance = distances[choice].get('example')
+
+            user_time = console.input(f"[cyan]Voer een {distance_label} tijd in (bijv. {example_distance}): [/cyan]")
+            predicted_times = calculate_predicted_time(user_time, from_distance, distances)
+
+            predicted_times_to_table(predicted_times)
+
+
+def show_menu(menu_items: dict) -> None:
+    for index, menu_item in menu_items.items():
+        print(f"{index}) {menu_item['label']}")
+    print("0) terug naar menu")
+
+
+def get_user_choice() -> int | None:
+    try:
+        return int(console.input("\n[bold][cyan]Maak een keuze: [/bold][/cyan]"))
+    except ValueError:
+        return None
 
 
 # # TODO functie wat opschonen, zodat hij overzichtelijker is: opsplitsen in meerdere functies
 
-def calculate_predicted_time(user_time: str, from_distance: float) -> dict[str, str]:
-    """Return predicted times as datetime.timedelta values."""
+def calculate_predicted_time(user_time: str, from_distance: float, distances: dict) -> dict[str, dict[str, str]]:
+    """Return predicted times as a dict"""
     time_in_seconds = to_seconds(user_time)
-    user_td = datetime.timedelta(seconds=time_in_seconds)
+    predicted_times: dict[str, dict[str, str]] = {}
 
-    predicted_times: dict[str, str] = {
-        '5km': format_timedelta(user_td),
-        '10km': format_timedelta(datetime.timedelta(seconds=est_time_in_seconds(10, time_in_seconds, from_distance))),
-        'Halve marathon': format_timedelta(datetime.timedelta(seconds=est_time_in_seconds(21.0975, time_in_seconds, from_distance))),
-        'Marathon': format_timedelta(datetime.timedelta(seconds=est_time_in_seconds(42.195, time_in_seconds, from_distance)))
-    }
+    for _, metric in distances.items():
+        predicted = est_time_in_seconds(metric['km'], time_in_seconds, from_distance)
+        formatted = format_timedelta(datetime.timedelta(seconds=predicted))
+        min_per_km = est_min_per_km(metric['km'], predicted)
+        metrics= {'time': formatted, 'pace': min_per_km}
+
+        predicted_times[metric['label']] = metrics
+
+    print()
 
     return predicted_times
 
-def est_time_in_seconds(km: float, time_in_seconds: int, from_distance: float) -> int:
-    return round(time_in_seconds * (km / from_distance) ** 1.06)
+
+def est_time_in_seconds(distance: float, time_in_seconds: int, from_distance: float) -> int:
+    return round(time_in_seconds * (distance / from_distance) ** 1.06)
+
+
+def est_min_per_km(distance: float, time: int) -> str:
+    pace_sec_per_km = time / distance
+    return f"{format_timedelta(datetime.timedelta(seconds=pace_sec_per_km))}/km"
 
 
 def predicted_times_to_table(predicted_times):
-    console = Console()
-    table = Table(title="Voorspelde tijden")
+    table = Table(title="🏃 Voorspelde tijden")
 
     table.add_column("Afstand", style="cyan")
     table.add_column("Tijd", style="green", justify="right")
+    table.add_column("Tempo", style="yellow", justify="right")
 
-    for distance, time in predicted_times.items():
-        table.add_row(distance, time)
+    for distance, metric in predicted_times.items():
+        table.add_row(distance, metric['time'], metric['pace'])
 
+    console.print()
     console.print(table)
-
-    input("Druk op enter om door te gaan")
+    input("Druk op enter om door te gaan...")
 
     return
 
 
-
-
 # TODO weghalen. is nu voor tesen fucties
-my_dict = {'5k': '20:00', '10k': '41:42', 'Halve marathon': '1:32:00', 'marathon': '3:11:49'}
+my_dict =  {'5km': {'time': '20:00', 'pace': '4:00/km'},
+            '10km': {'time': '41:42', 'pace': '4:10/km'},
+            'Halve marathon': {'time': '1:32:00', 'pace': '4:21/km'},
+            'Marathon': {'time': '3:11:49', 'pace': '4:32/km'}
+            }
+
 # predicted_times_to_table(my_dict)
 calculate_performances()
 # to_seconds("00:20:00")
